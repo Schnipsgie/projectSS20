92a512b2729a44c924a8f0339dc6334c6f282431
Schnipsgie <Rene-Priel@t-online.de>
2020-07-03 14:24:04 +0200

    Added Button

--- Project/src/main.java
+++ Project/src/main.java
@@ -1,8 +1,14 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.util.Vector;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 
 public class main {
@@ -11,16 +17,14 @@ public class main {
 
 		Vector<Rezept> rezeptListe = new Vector<Rezept>();
 		
-		Vector<String> zuVec = new Vector<String>();
-		zuVec.addElement("Eier");
-		zuVec.addElement("Salat");
-		Rezept r = new Rezept("Eiersalat", zuVec);
+//		Vector<String> zuVec = new Vector<String>();
+//		zuVec.addElement("Eier");
+//		zuVec.addElement("Salat");
+//		Rezept r = new Rezept("Eiersalat", zuVec);
 		
 		//initial Loading
 		FileHandler FH = new FileHandler();
-//		FH.writeFile("Kuchen ; Eier ; Mehl ; Zucker");
-//		FH.writeFile("Pizza ; Mehl ; Salz ; Schinken ; Salami ");
-//		FH.writeFile("Spiegelei ; Eier ; Öl ; Salz");
+
 		Vector<String> rezeptDataList = FH.readFile();
 		for (String rezeptData : rezeptDataList) {
 			//System.out.println(rezeptLine);
@@ -30,10 +34,13 @@ public class main {
 			
 		
 //Sollte in eine extra Klasse
+		//Hauptfester erstellen
 		JFrame fenster = new JFrame("Rezepte");
-		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Programm wird beendet wenn es geschlossen wird
 		fenster.setSize(600, 250);
-       
+		GridLayout gridlayout = new GridLayout(2, 1); //Layout festlegen
+	    gridlayout.setVgap(10); // Abstand zwischen den Grids
+	    fenster.setLayout(gridlayout); //Grid zum Hauptfenster hinzufügen
       
         
         //Basisliste an rezepten
@@ -56,11 +63,20 @@ public class main {
         columnNames.addElement("Column 5");
         columnNames.addElement("Column 6");
        
+        JPanel controlPanel = new JPanel();
         
+        
+        JButton addButton = new JButton("Add");
+        addButton.setSize(30, 30);
+        controlPanel.add(addButton);
         
         JTable table = new JTable (tableData, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
-        fenster.add(scrollPane, BorderLayout.CENTER);
+
+        fenster.add(scrollPane);
+        fenster.add(controlPanel);
+    
+     
         fenster.setVisible(true);
         
 	}
