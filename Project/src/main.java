dead7241c738d41845fbb8a63b48609f045d89eb
Neme1337 <67865608+Neme1337@users.noreply.github.com>
2020-07-29 21:38:35 +0200

    Final

--- Project/src/main.java
+++ Project/src/main.java
@@ -4,18 +4,18 @@ import javax.swing.text.TabExpander;
 
 //Requirements:
 //
-//DONE	1)Der Benutzer soll neue Objekte anlegen können.
+//		1)Der Benutzer soll neue Objekte anlegen können.
 //		2)Objekte sollen anhand des Namens eindeutig identifizierbar sein.
-//DONE	3)Objekte sollen folgende Bestandteile besitzen: eindeutiger Name,Freitext und eine beliebige Anzahl an Elementen mit Mengenangabe, Einheit und Bezeichnung.
-//DONE?	4)Der Benutzer soll Objekte anhand der Bestandteile suchen können.
-//DONE	5)Der Benutzer soll Objekte löschen können.
+//		3)Objekte sollen folgende Bestandteile besitzen: eindeutiger Name,Freitext und eine beliebige Anzahl an Elementen mit Mengenangabe, Einheit und Bezeichnung.
+//		4)Der Benutzer soll Objekte anhand der Bestandteile suchen können.
+//		5)Der Benutzer soll Objekte löschen können.
 //		6)Der Benutzer soll Objekte bearbeiten können.
-//so halb7)Der Benutzer soll ein Objekt auswählen können,um diesesanzuzeigen.
-//DONE	8)Die Anwendung soll eine Übersichtsliste aller Objekte anzeigen.
-//DONE	9)Die Anwendung soll beim Start automatisch alle gespeicherten Daten laden.
-//so halb		10)Die Anwendung soll eine grafische Übersicht über die nach Menge absteigend sortierten Elementen ausgeben können.
-//DONE?			Bei großer Anzahl unterschiedlicherElemente sollen nur die häufigstenangezeigt werden. 
-//DONE			Dies soll in Form eines Balkendiagramms geschehen.
+//		7)Der Benutzer soll ein Objekt auswählen können,um diesesanzuzeigen.
+//		8)Die Anwendung soll eine Übersichtsliste aller Objekte anzeigen.
+//		9)Die Anwendung soll beim Start automatisch alle gespeicherten Daten laden.
+//		10)Die Anwendung soll eine grafische Übersicht über die nach Menge absteigend sortierten Elementen ausgeben können.
+//		Bei großer Anzahl unterschiedlicherElemente sollen nur die häufigstenangezeigt werden. 
+//		Dies soll in Form eines Balkendiagramms geschehen.
 
 public class main {
 
